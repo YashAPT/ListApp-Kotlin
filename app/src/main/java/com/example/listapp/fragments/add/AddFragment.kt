d5262d4c05ed495c9aaf4ce8f7213a8f7280443b
yash <yashkumar201301@gmail.com>
2021-02-09 12:57:43 +0530

    Shared ViewModel

--- app/src/main/java/com/example/listapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/listapp/fragments/add/AddFragment.kt
@@ -1,15 +1,26 @@
 package com.example.listapp.fragments.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.listapp.R
+import com.example.listapp.data.models.Priority
+import com.example.listapp.data.models.ToDoData
+import com.example.listapp.data.viewmodel.ToDoViewModel
 import com.example.listapp.databinding.FragmentAddBinding
+import com.example.listapp.fragments.SharedViewModel
 
 class AddFragment : Fragment() {
 
     private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -20,12 +31,40 @@ class AddFragment : Fragment() {
         _binding = FragmentAddBinding.inflate(layoutInflater, container, false)
         setHasOptionsMenu(true)
 
+        binding.prioritiesSpinner.onItemSelectedListener = mSharedViewModel.listener
+
         return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_add) {
+            insertData()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertData() {
+        val mTitle = binding.titleEt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEt.text.toString()
+
+        val validation = mSharedViewModel.verifyData(mTitle, mDescription)
+        if (validation) {
+            val newData =
+                ToDoData(0, mTitle, mSharedViewModel.parsePriority(mPriority), mDescription)
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "ADDED...", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "EMPTY FIELD(S)...", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
